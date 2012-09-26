3bf187f93d5d86477d2d329026491319411c9fd5
Mohamed Wael Khobalatte <wael.khobalatte@gmail.com>
2012-09-26 02:01:50 +0100

    Major progress

--- imac/assets/js/base.js
+++ imac/assets/js/base.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
   console.log("loc is" + loc);
 
 	$("#filter li a").each(function() {
- 		   if(this.href.split("/")[11] == loc.split("/")[11]) {
+ 		   if(this.href.split("/")[3] == loc.split("/")[3]) {
 
-        console.log("this href when split is" + this.href.split("/")[11]);
-        console.log("tloc when split is" + loc.split("/")[11]);
+        console.log("this href when split is" + this.href.split("/")[3]);
+        console.log("tloc when split is" + loc.split("/")[3]);
 
      		$(this).parent().addClass('active');
 
